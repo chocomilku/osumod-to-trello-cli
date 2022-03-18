3a8d3da45baf0b5d1e708367a6e4f77aea6a11bf
chocomilku <andrei.berja078@gmail.com>
2022-03-18 23:41:01 +0800

    fixed problem. dont know how

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import puppeteer from "puppeteer";
 const scrape = async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	await page.goto("https://example.com");
-	await page.screenshot({ path: "example.png" });
+	await page.goto("https://osumod.com/chocomilku-");
+	const element = await page.waitForSelector(
+		"div.ant-card:nth-child(1) > div:nth-child(3) > div:nth-child(7)"
+	);
+	const text = await page.evaluate((element) => element.textContent, element);
+	console.log(text);
 
 	await browser.close();
 };
