612580cd1f03693f6b308c91c4b8b085fde97d5c
chocomilku <andrei.berja078@gmail.com>
2022-03-19 12:38:26 +0800

    refactored code to use a class instead of functional approach

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,38 @@
 import puppeteer from "puppeteer";
 
-const scrape = async () => {
-	try {
-		const browser = await puppeteer.launch({ headless: false });
-		const page = await browser.newPage();
-		await page.goto("https://osumod.com/chocomilku-", {
-			waitUntil: "networkidle0",
-		});
-		const data = await page.evaluate(
-			() => document.querySelector(".RequestList-container")?.innerHTML
-		);
+class Scraper {
+	private readonly username: string;
+	private _html: string | undefined = "";
 
-		console.log(data);
-
-		await browser.close();
-	} catch (err) {
-		console.error(err);
+	constructor(username: string) {
+		this.username = username;
 	}
-};
 
-scrape();
+	private async scrape() {
+		try {
+			const browser = await puppeteer.launch({ headless: false });
+			const page = await browser.newPage();
+			await page.goto(`https://osumod.com/${this.username}`, {
+				waitUntil: "networkidle0",
+			});
+			const data = await page.evaluate(
+				() => document.querySelector(".RequestList-container")?.innerHTML
+			);
+
+			this._html = data;
+
+			await browser.close();
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	public async html() {
+		await this.scrape();
+		return this._html;
+	}
+}
+
+const a = new Scraper("chocomilku").html();
+
+console.log(a);
