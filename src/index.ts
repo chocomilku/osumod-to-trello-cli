8deedcf2964648c3d207578af84c945e55f77780
chocomilku <andrei.berja078@gmail.com>
2022-03-19 20:44:07 +0800

    able to scrape now
    aaaaaaaaaaaahhhhhh. the comments are left out since they have no identifier with each other

--- src/index.ts
+++ src/index.ts
@@ -14,16 +14,49 @@ const scraper = new Scraper(user).html();
 	if (!data) throw new Error("help");
 	const $ = cheerio.load(data);
 
-	$(".RequestList-container").each((i, e) => {
-		console.log("index index %d", i);
-		console.log(e);
-		$(e)
-			.children()
-			.each((index, el) => {
-				const d = $(el).text();
+	const cards = [];
 
-				console.log(index);
-				console.log(d);
-			});
-	});
+	const totalCards = $(".RequestList-container").children().length;
+
+	for (let i = 0; i < totalCards; i++) {
+		const path = `.RequestList-container > .ant-card:nth-child(${i + 1})`;
+
+		const title = $(`${path} > .ant-card-body > .MapCard-primary`).text();
+
+		const artist = $(
+			`${path} > .ant-card-body > .MapCard-row:nth-child(2)`
+		).text();
+
+		const mapper = $(
+			`${path} > .ant-card-body > .MapCard-row:nth-child(3)`
+		).text();
+
+		const time = $(
+			`${path} > .ant-card-body > .MapCard-attr-list > .MapCard-attr:nth-child(1)`
+		).text();
+		const bpm = $(
+			`${path} > .ant-card-body > .MapCard-attr-list > .MapCard-attr:nth-child(2)`
+		).text();
+
+		const status = $(
+			`${path} > .ant-card-head > .ant-card-head-wrapper > .ant-card-head-title > .MapCard-title > div:nth-child(1) > span:nth-child(2)`
+		).text();
+
+		const modType = $(
+			`${path} > .ant-card-head > .ant-card-head-wrapper > .ant-card-head-title > .MapCard-title > .MapCard-mod-type`
+		).text();
+
+		const final = {
+			index: i,
+			title,
+			artist,
+			mapper,
+			time,
+			bpm,
+			status,
+			modType,
+		};
+		cards.push(final);
+	}
+	console.log(cards);
 })();
