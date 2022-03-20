5fc1bf968b931e8b45e3e8bdb5f64fe53ea40311
chocomilku <andrei.berja078@gmail.com>
2022-03-20 14:19:07 +0800

    moved osumodCards function to another file

--- src/process.ts
+++ src/process.ts
@@ -1,8 +1,4 @@
-import "dotenv/config";
-import { Scraper } from "./scraper";
-import fetch from "axios";
 import * as cheerio from "cheerio";
-import { CheerioAPI } from "cheerio";
 
 export interface cardsType {
 	index: number;
@@ -17,56 +13,61 @@ export interface cardsType {
 	img: string | undefined;
 }
 
-class ProcessData {
-	constructor(data: string) {
-		this.$ = cheerio.load(data);
-	}
+export const osumodCards = async (
+	scraper: Promise<string | undefined>
+): Promise<cardsType[] | void> => {
+	try {
+		// extract html from scraper class
+		const data = await scraper;
 
-	protected readonly $;
-	public allCards: cardsType[] = this.getData();
+		// check if there's no data
+		if (!data) throw new Error("help");
 
-	public totalCards(): number {
-		return this.$(".RequestList-container").children().length;
-	}
+		// load the data to cheerio
+		const $ = cheerio.load(data);
 
-	private getData(): cardsType[] {
-		const cards: cardsType[] = [];
-		for (let i = 0; i < this.totalCards(); i++) {
+		// final array to be returned
+		const cards = [];
+
+		// get the total number of cards present at osumod
+		const totalCards = $(".RequestList-container").children().length;
+
+		// iterate through each cards and get the specific data
+		for (let i = 0; i < totalCards; i++) {
 			const path = `.RequestList-container > .ant-card:nth-child(${i + 1})`;
 
-			const title = this.$(
-				`${path} > .ant-card-body > .MapCard-primary`
-			).text();
+			const title = $(`${path} > .ant-card-body > .MapCard-primary`).text();
 
-			const artist = this.$(
+			const artist = $(
 				`${path} > .ant-card-body > .MapCard-row:nth-child(2)`
 			).text();
 
-			const mapper = this.$(
+			const mapper = $(
 				`${path} > .ant-card-body > .MapCard-row:nth-child(3)`
 			).text();
 
-			const time = this.$(
+			const time = $(
 				`${path} > .ant-card-body > .MapCard-attr-list > .MapCard-attr:nth-child(1)`
 			).text();
-			const bpm = this.$(
+			const bpm = $(
 				`${path} > .ant-card-body > .MapCard-attr-list > .MapCard-attr:nth-child(2)`
 			).text();
 
-			const status = this.$(
+			const status = $(
 				`${path} > .ant-card-head > .ant-card-head-wrapper > .ant-card-head-title > .MapCard-title > div:nth-child(1) > span:nth-child(2)`
 			).text();
 
-			const modType = this.$(
+			const modType = $(
 				`${path} > .ant-card-head > .ant-card-head-wrapper > .ant-card-head-title > .MapCard-title > .MapCard-mod-type`
 			).text();
 
-			const urlB = this.$(`${path} > .ant-card-cover > a`).attr("href");
+			const urlB = $(`${path} > .ant-card-cover > a`).attr("href");
 
 			const url = urlB?.replace(`/s`, `/beatmapsets`);
 
-			const img = this.$(`${path} > .ant-card-cover > a > img`).attr("src");
+			const img = $(`${path} > .ant-card-cover > a > img`).attr("src");
 
+			// final structure of osumod cards
 			const final: cardsType = {
 				index: i,
 				title,
@@ -80,19 +81,12 @@ class ProcessData {
 				img,
 			};
 
+			// push the data to the cards array
 			cards.push(final);
 		}
+		console.log(cards);
 		return cards;
+	} catch (err) {
+		console.error(err);
 	}
-}
-
-(async () => {
-	const yes = new Scraper("chocomilku-").html();
-	const data = await yes;
-
-	if (!data) throw new Error("help");
-
-	const test = new ProcessData(data);
-	console.log(test.totalCards);
-	console.log(test.allCards);
-})();
+};
